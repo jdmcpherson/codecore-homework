b29e6ec6e7a73978eb5b49c961473eebcc6eddfa
James McPherson <mcpherson.jamesd@gmail.com>
2017-09-25 15:57:56 -0700

    Completed Task

--- question_1.rb
+++ question_1.rb
@@ -1,17 +1,20 @@
 module HelperMethods 
+    class Title
+        def initialize(string) 
+            @string = string
+        end 
 
-    def initialize(string) 
-        @string = string
-    end 
-
-    def titleize
-        array = @string.split(' ')
-        output = ""
-        for word in array
-            unless word == 'in' || word == 'the' || word == 'of' || word == 'and' || word == 'or' || word || from do
-                output += word.capitalize
+        def titleize
+            array = @string.split(' ')
+            output = ""
+            for word in array
+                unless word == 'in' || word == 'the' || word == 'of' || word == 'and' || word   == 'or' || word == 'from'
+                    output += "#{word.capitalize} "
+                else
+                    output +="#{word} "
+                end
             end
+           print output
         end
-        print output
     end
 end
