d77e735d96cb16fb0d0197f851120b2dfb8502a1
jdmcpherson <31864624+jdmcpherson@users.noreply.github.com>
2017-10-16 19:15:03 -0700

    Updated with feedback

--- question_3.rb
+++ question_3.rb
@@ -1,14 +1,12 @@
 def is_prime(num)
     value = Math::sqrt(num.to_f)
-
-    prime_status = true
-
+    
     for number in 2...value
         check = value/number
         if check % 1 === 0
-            prime_status = false
+            puts false
         end
     end
 
-    puts prime_status
+    puts true
 end
