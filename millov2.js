9c932e22c17bfb9166149bb984774696ffc1efe0
James McPherson <mcpherson.jamesd@gmail.com>
2017-10-25 22:48:09 -0700

    Update

--- millov2.js
+++ millov2.js
@@ -1,57 +1,109 @@
-class Task {
-    constructor(task, name) {
-        this.task = task;
-        this.name = name;
+    class Task {
+        constructor(task, person) {
+            this.task = task;
+            this.person = person;
+        }
+
+        render() {
+            let output = `${this.task}`;
+            if (this.person) {
+                output += ` • ${this.person}`
+            };
+            return output;
+        }
     }
 
-    render() {
-         let output = `${this.task}`;
-         if (this.name) {
-             output += ` • ${this.name}`
-         };
-         return console.log(output);
-    }
-}
+    class List {
+        constructor(name) {
+            this.name = name;
+            this.tasks = [];
+        }
 
-class List {
-    constructor(list) {
-        this.list = list;
-        this.tasks = [];
-    }
+        addTask(task) {
+            this.tasks.push(task);
+            return `Added task "${task.task}" to list "${this.name}"`;
+        }
 
-    addTask(task) {
-        let newTask = task
-        this.tasks.push(newTask);
-        return `Added task "${newTask.task}" to list "${this.list}"`;
-    }
+        removeTask(removeTask) {
+            let index = this.tasks.indexOf(removeTask);
+            for (let task of this.tasks) {
+                if (task.task == removeTask) {
+                    let result = task;
+                    this.tasks.splice(index, 1)
+                    // console.log(`Removed task "${removeTask} from "${this.name}"`);
+                    return result
+                }
+            } 
+            return `Task not found`
+        }
 
-    removeTask(task) {
-
-    }
-
-    render() {
-
-    }
-}
-
-class Board {
-    constructor(board) {
-
-    }
-
-    addList(list) {
-
-    }
-        
-    removeList(listToRemove) {
-
-    }
-        
-    render() {
-
+        render() {
+            let result = ``;
+            let counter = 1;
+            result += `|----------\n`;
+            result += `| ${this.name}\n`;
+            result += `|----------\n`;
+            for (let task of this.tasks) {
+                result += `| ${counter}: ${task.render()}\n`;
+                counter++;
+            }
+            return result;
+        }
     }
 
-    moveTaskTo(task, fromList, toList) {
+    class Board {
+        constructor(name) {
+            this.name = name;
+            this.lists = [];
+        }
 
+        addList(list) {
+            this.lists.push(list);
+            return `Added list "${list.name}" to board "${this.name}"`;
+        }
+
+        removeList(removeList) {
+            let index = this.lists.indexOf(removeList);
+            for (let list of this.lists) {
+                if (list.name == removeList) {
+                    let result = list;
+                    this.lists.splice(index, 1)
+                    console.log(`Removed list "${removeList} from "${this.name}"`);
+                    return result;
+                }
+            } 
+            return `Task not found`
+        }
+
+        render() {
+            let result = ``;
+            result += `|************\n`
+            result += `| *${this.name}*\n`
+            result += `|************\n`
+            for (let list of this.lists) {
+                result += `${list.render()}\n`;
+            }
+            return result;
+        }
+
+        moveTaskTo(task, moveFromList, moveToList) {
+            let toList = null;
+            let fromList = null;
+            for (let i = 0; i < this.lists.length; i++) {
+                // console.log(this.lists[i])
+                if (this.lists[i].name  === moveFromList) {
+                    // console.log(this.lists[i]);
+                    fromList = this.lists[i];
+                } else if (this.lists[i].name === moveToList) {
+                    // console.log(this.lists[i])
+                    toList = this.lists[i];
+                }
+            }
+            if (toList != null && fromList != null) {
+                toList.addTask(fromList.removeTask(task));
+                return `Moved task ${task} from ${moveFromList} to ${moveToList}`
+            } else {
+                return "A list was not found";
+            }
+        }
     }
-}
